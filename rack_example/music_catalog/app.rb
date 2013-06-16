054bb114af327426b6b5835dcadc16c380a20c83
matthewhersh <mattmhersh@gmail.com>
2013-06-15 23:41:31 -0400

    Updated Content.

--- rack_example/music_catalog/app.rb
+++ rack_example/music_catalog/app.rb
@@ -7,7 +7,7 @@ set :markdown, :layout_engine => :haml
 
 class MusicCatalog < Sinatra::Base
  get "/" do
-   markdown :index, :layout => false
+   markdown :index, :layout => :layout2
  end
 
  get "/about" do
