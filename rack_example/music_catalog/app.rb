8ec0d995f7ed54761290341aba33ade62cbcc978
matthewhersh <mattmhersh@gmail.com>
2013-06-16 00:58:33 -0400

    Updated Content.

--- rack_example/music_catalog/app.rb
+++ rack_example/music_catalog/app.rb
@@ -7,7 +7,7 @@ set :markdown, :layout_engine => :haml, :format => :html5
 
 class MusicCatalog < Sinatra::Base
  get "/" do
-   send_file File.join('public', 'index.html')
+   send_file File.join('public', '/views/index.html')
  end
 
  get "/about" do
